55faa03a29a916804e1f7b9ea2db3a51198b2089
Brian Grenier <grenierb96@gmail.com>
2024-10-25 11:39:36 -0600

    stuff

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+
+if [[ -z "${PREFIX_API_KEY}" ]]; then
+    echo "api key not present in env"
+    exit 1
+fi
+
 CONDA_BLD_PATH="output"
 
 (magic run clean ; magic run update_and_build) || exit 1
 
 for file in "$CONDA_BLD_PATH"/**/*.conda; do
-    rattler-build upload prefix -c "mojo-community-nightly" "$file" --api-key=$PREFIX_API_KEY || (echo "upload failed" ; exit 1)
+    magic run rattler-build upload prefix -c "mojo-community-nightly" "$file" || (echo "upload failed" ; exit 1)
 done
 
 magic run clean
